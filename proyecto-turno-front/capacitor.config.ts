8cff3077f6c11f567e38ffe33c9b6fc74331dff8
Kevin Cuesta <CRACKESAN@GMAIL.COM>
2024-05-16 16:54:10 -0500

    Creacion Proyecto Front

---
+++ proyecto-turno-front/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyecto-turno',
+  webDir: 'dist'
+};
+
+export default config;
